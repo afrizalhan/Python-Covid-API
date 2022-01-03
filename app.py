58139c55287d5fbd0246b90b966d3aaba79cfe20
afrizalhan <afrizalraehan6@gmail.com>
2022-01-03 22:16:11 +0700

    finished tenth contract

--- app.py
+++ app.py
@@ -111,6 +111,20 @@ def findDailyYearBased(year):
     }
     return jsonify(response)
 
+@app.route("/daily/<year>/<month>/", methods = ["GET"])
+def findDailyMonthBased(year, month):
+    since = request.args.get('since', default = '2020.03.02', type = str)
+    upto = request.args.get('upto', default = None, type = str)
+
+    data = get_daily_month_based(year, month, since, upto)
+
+    response = {
+        "ok": True,
+        "data": data,
+        "message": "Data retrieved successfully",
+    }
+    return jsonify(response)
+
 @app.route("/daily/<year>/<month>/<day>")
 def findDailyBased(year, month, day):
     data = get_daily_based(year, month, day)
